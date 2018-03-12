5a3a9b71eb0e61b2583521a1f9b9afb0555c723f
almoller <moller.alan@gmail.com>
2018-03-11 23:54:54 -0600

    Now generating functional buttons

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,29 @@
-$(".button").on("click", function() {
-    // $(".gif_ImagesSection").append(src.)
+
+
+var topics = ["snow", "rain", "Oh Brother", "LOL", "disney"];
+
+function renderButtons() {
+    $("#buttonSection").empty;
+
+    for (var i = 0; i < topics.length; i++) {
+        var a = $("<button>");
+        a.addClass("topicButton");
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+        $("#buttonSection").append(a);
+    }
+}
+
+function displayTopicGif () {
+
+    alert("BOO!");// $(".gif_ImagesSection").append(src.)
     var imgUrl = "assets/images/q9_sugarloaf.jpg";
     var $img = $("<img>").attr("src", imgUrl);  // Creates an element to hold the image
-    $(".gif_ImageSection").append($img);
-})
+    $("#gif_ImageSection").append($img);
+
+
+};
+
+$(document).on("click", ".topicButton", displayTopicGif);
+
+renderButtons();
