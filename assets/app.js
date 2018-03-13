1a7b68dbfce7e0081257564fa3e8c32e90197b99
almoller <moller.alan@gmail.com>
2018-03-12 21:45:49 -0600

    still gifs working

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,7 @@
 var topics = ["snow", "rain", "Oh Brother", "LOL", "disney"];
 
 function renderButtons() {
-    $("#buttonSection").empty;
+    $("#buttonSection").empty();
 
     for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
@@ -14,14 +14,35 @@ function renderButtons() {
     }
 }
 
-function displayTopicGif () {
+function displayTopicGif() {
+    $("#starterImg").hide();
 
-    alert("BOO!");// $(".gif_ImagesSection").append(src.)
-    var imgUrl = "assets/images/q9_sugarloaf.jpg";
-    var $img = $("<img>").attr("src", imgUrl);  // Creates an element to hold the image
-    $("#gif_ImageSection").append($img);
+    var topic = $(this).attr("data-name");
+    var key = "7wHvqslBWYbnRRNpNRV73m1WyvYdKXRo";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + key
+        + "&q=" + topic + "&limit=10";
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
 
+        for (var j = 0; j < response.data.length; j++) {
+            var gifDiv = $("<div class='gif'>");
+
+            var rated = response.data[j].rating;   
+            var $rated = $("<p>").text("Rating: " + rated);   
+            
+            var imgUrl = response.data[j].images.fixed_width_still.url;
+            var $img = $("<img>").attr("src", imgUrl);
+
+            gifDiv.append('<hr>', $rated, $img);
+
+            $("#gif_ImageSection").append(gifDiv);
+        };
+
+    });
 };
 
 $(document).on("click", ".topicButton", displayTopicGif);
